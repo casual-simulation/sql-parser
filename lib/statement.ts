10be2a0cd5054050084c81fd5958a5a34628212e
Kallyn Gowdy <kallyngowdy@gmail.com>
2025-10-03 14:01:22 -0400

    Finish Query interface

--- lib/statement.ts
+++ lib/statement.ts
@@ -19,13 +19,211 @@ export type Statement = Query;
  * @see https://docs.rs/sqlparser/latest/sqlparser/ast/struct.Query.html
  */
 export interface Query {
-    // with?:
+    with?: With;
 
     /**
      * SELECT or UNION / EXCEPT / INTERSECT
      */
     body: SetExpr;
+
+    /**
+     * `ORDER BY` clause.
+     */
+    order_by?: OrderBy;
+
+    /**
+     * `LIMIT ... OFFSET ... | LIMIT <offset>, <limit>` clause.
+     */
+    limit_clause?: LimitClause,
+
+    /**
+     * `FETCH { FIRST | NEXT } <N> [ PERCENT ] { ROW | ROWS } | { ONLY | WITH TIES }`
+     */
+    fetch?: Fetch,
+
+    /**
+     * `FOR { UPDATE | SHARE } [ OF table_name ] [ SKIP LOCKED | NOWAIT ]`
+     */
+    locks: LockClause[],
+
+    /**
+     * `FOR XML { RAW | AUTO | EXPLICIT | PATH } [ , ELEMENTS ]`
+     * `FOR JSON { AUTO | PATH } [ , INCLUDE_NULL_VALUES ]` (MSSQL-specific)
+     * 
+     * @see https://docs.rs/sqlparser/latest/sqlparser/ast/enum.ForClause.html
+     */
+    for_clause?: unknown,
+
+    /**
+     * [ClickHouse syntax](https://clickhouse.com/docs/sql-reference/statements/select#settings-in-select-query): `SELECT * FROM t SETTINGS key1 = value1, key2 = value2`
+     * 
+     * @see https://docs.rs/sqlparser/latest/sqlparser/ast/struct.Setting.html
+     */
+    settings?: unknown[],
+
+    /**
+     * `SELECT * FROM t FORMAT JSONCompact`
+     * [ClickHouse](https://clickhouse.com/docs/en/sql-reference/statements/select/format) (ClickHouse-specific)
+     * 
+     * @see https://docs.rs/sqlparser/latest/sqlparser/ast/enum.FormatClause.html
+     */
+    format_clause?: unknown,
+
+    /**
+     * [Pipe Syntax](https://cloud.google.com/bigquery/docs/reference/standard-sql/pipe-syntax#pipe_syntax).
+     * 
+     * @see https://docs.rs/sqlparser/latest/sqlparser/ast/enum.PipeOperator.html
+     */
+    pipe_operators: unknown[],
 }
+
+/**
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/struct.OrderBy.html
+ */
+export interface OrderBy {
+    kind: OrderByKind;
+    interpolate?: Interpolate;
+}
+
+/**
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/enum.OrderByKind.html
+ */
+export type OrderByKind = {
+    /**
+     * `ALL` syntax of [DuckDB](https://duckdb.org/docs/sql/query_syntax/orderby) and [ClickHouse](https://clickhouse.com/docs/en/sql-reference/statements/select/order-by).
+     */
+    All?: OrderByOptions;
+
+    /**
+     * Expressions.
+     */
+    Expressions?: OrderByExpr[];
+}
+
+/**
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/struct.Interpolate.html
+ */
+export interface Interpolate {
+    exprs?: InterpolateExpr[];
+}
+
+/**
+ * ClickHouse `INTERPOLATE` clause for use in `ORDER BY` clause when using `WITH FILL` modifier.
+ * Supported by [ClickHouse syntax](https://clickhouse.com/docs/en/sql-reference/statements/select/order-by#order-by-expr-with-fill-modifier)
+ * 
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/struct.InterpolateExpr.html
+ */
+export interface InterpolateExpr {
+    column: Ident;
+    expr?: Expr;
+}
+
+
+/**
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/struct.Fetch.html
+ */
+export interface Fetch {
+    with_ties: boolean;
+    percent: boolean;
+    quantity?: Expr;
+}
+
+/**
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/struct.LimitClause.html
+ */
+export type LimitClause = {
+    /**
+     * Standard SQL syntax
+
+        `LIMIT <limit> [BY <expr>,<expr>,...] [OFFSET <offset>]`
+     */
+    LimitOffset?: {
+        limit?: Expr,
+        offset?: Offset,
+        limit_by: Expr[],
+    },
+
+    /**
+     * [MySQL-specific](https://dev.mysql.com/doc/refman/8.4/en/select.html) syntax; the order of expressions is reversed.
+     */
+    OffsetCommaLimit?: {
+        offset: Expr,
+        limit: Expr,
+    },
+}
+
+/**
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/struct.Offset.html
+ */
+export interface Offset {
+    value: Expr;
+    rows: OffsetRows;
+}
+
+/**
+ * Stores the keyword after `OFFSET <number>`
+ * 
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/enum.OffsetRows.html
+ */
+export type OffsetRows = 
+    | 'None'
+    | 'Row'
+    | 'Rows';
+
+/**
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/struct.LockClause.html
+ */
+export interface LockClause {
+    lock_type: LockType;
+    of?: ObjectName;
+    nonblock?: NonBlock;
+}
+
+/**
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/enum.LockType.html
+ */
+export type LockType = 'Share' | 'Update';
+
+/**
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/enum.NonBlock.html
+ */
+export type NonBlock = 'Nowait' | 'SkipLocked';
+
+/**
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/struct.With.html
+ */
+export interface With {
+    /**
+     * Token for the “WITH” keyword
+     */
+    with_token: AttachedToken;
+    recursive: boolean;
+    cte_tables: Cte[];
+}
+
+/**
+ * A single CTE (used after `WITH`): `<alias> [(col1, col2, ...)] AS <materialized> ( <query> )` The names in the column list before AS, when specified, replace the names of the columns returned by the query. The parser does not validate that the number of columns in the query matches the number of columns in the query.
+ * 
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/struct.Cte.html
+ */
+export interface Cte {
+    alias: TableAlias;
+    query: Query;
+    from?: Ident;
+    materialized?: CteAsMaterialized;
+
+    /**
+     * Token for closing parenthesis.
+     */
+    closing_paren_token: AttachedToken;
+}
+
+/**
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/enum.CteAsMaterialized.html
+ */
+export type CteAsMaterialized = 
+    | 'Materialized'
+    | 'NotMaterialized';
 
 /**
  * A node in a tree, representing a "query body" expression.
@@ -41,9 +239,83 @@ export interface SetExpr {
      * The `SELECT` statement.
      */
     Select?: Select;
+
+    /**
+     * Parenthesized SELECT subquery, which may include more set operations in its body and an optional ORDER BY / LIMIT.
+     */
+    Query?: Query;
+
+    /**
+     * UNION/EXCEPT/INTERSECT of two queries
+     */
+    SetOperation?: {
+        op: SetOperator;
+        set_quantifier: SetQuantifier;
+        left: SetExpr;
+        right: SetExpr;
+    };
+
+    Values?: Values;
+
+    Insert?: Statement;
+    Update?: Statement;
+    Delete?: Statement;
+    Merge?: Statement;
+    Table?: Table;
 } //Select | SetOperation | Values | Query | Insert | Update | Delete | CreateTable | CreateView | CreateIndex | AlterTable | Drop | Truncate | Explain | ShowColumns | ShowTables | UseStatement | Grant | Revoke | StartTransaction | Commit | Rollback | Prepare | Execute | Deallocate;
 
+/**
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/enum.SetOperator.html
+ */
+export type SetOperator = 
+    | 'Union'
+    | 'Except'
+    | 'Intersect'
+    | 'Minus';
 
+/**
+ * A quantifier for SetOperator.
+ * 
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/enum.SetQuantifier.html
+ */
+export type SetQuantifier =
+    | 'All'
+    | 'Distinct'
+    | 'ByName'
+    | 'AllByName'
+    | 'DistinctByName'
+    | 'None';
+
+/**
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/struct.Values.html
+ */
+export interface Values {
+    /**
+     * The list of rows to insert.
+     */
+    rows: Row[];
+}
+
+/**
+ * A single row of values to insert.
+ */
+export interface Row {
+    /**
+     * Was there an explicit ROWs keyword (MySQL)? https://dev.mysql.com/doc/refman/8.0/en/values.html
+     */
+    explicit_row: boolean;
+    rows: Expr[][];
+}
+
+/**
+ * A [TABLE command](https://www.postgresql.org/docs/current/sql-select.html#SQL-TABLE).
+ * 
+ * @see https://docs.rs/sqlparser/latest/sqlparser/ast/struct.Table.html
+ */
+export interface Table {
+    table_name?: string;
+    schema_name?: string;
+}
 
 /**
  * A select statement.
@@ -60,7 +332,7 @@ export interface Select {
     into?: SelectInto;
     from: TableWithJoins[];
     // TODO: Define proper type
-    lateral_views: any[];
+    lateral_views: unknown[];
     prewhere?: Expr;
     selection?: Expr;
     group_by: GroupByExpr;
@@ -73,8 +345,8 @@ export interface Select {
     window_before_qualify: boolean;
 
     // TODO: Define proper type
-    value_table_mode?: any;
-    connect_by?: any;
+    value_table_mode?: unknown;
+    connect_by?: unknown;
     flavor: SelectFlavor;
 }
 
@@ -153,7 +425,7 @@ export type SelectItemQualifiedWildcardKind = {
  * 
  * @see https://docs.rs/sqlparser/latest/sqlparser/ast/struct.WildcardAdditionalOptions.html
  */
-export type WildcardAdditionalOptions = any; // TODO: Docs
+export type WildcardAdditionalOptions = unknown; // TODO: Docs
 
 /**
  * Snowflake `EXCLUDE` information.
@@ -199,9 +471,9 @@ export type TableFactor = {
         version?: TableVersion,
         with_ordinality: boolean,
         partitions: Ident[],
-        json_path?: any,
-        sample?: any,
-        index_hints: any[],
+        json_path?: unknown,
+        sample?: unknown,
+        index_hints: unknown[],
     },
     Derived?: {
         lateral: boolean,
@@ -229,14 +501,14 @@ export type TableFactor = {
         json_expr: Expr,
         json_path: Value,
         // TODO: Define proper type
-        columns: any[],
+        columns: unknown[],
         alias?: TableAlias,
     },
     OpenJsonTable?: {
         json_expr: Expr,
         json_path?: Value,
         // TODO: Define proper type
-        columns: any[],
+        columns: unknown[],
         alias?: TableAlias,
     },
     NestedJoin?: {
@@ -264,19 +536,19 @@ export type TableFactor = {
         table: TableFactor,
         partition_by: Expr[],
         order_by: OrderByExpr[],
-        measures: any[],
-        rows_per_match?: any,
-        after_match_skip?: any,
-        pattern: any,
-        symbols: any[],
+        measures: unknown[],
+        rows_per_match?: unknown,
+        after_match_skip?: unknown,
+        pattern: unknown,
+        symbols: unknown[],
         alias?: TableAlias,
     },
     XmlTable?: {
         // TODO: Define proper types
-        namespaces: any[],
+        namespaces: unknown[],
         row_expression: Expr,
-        passing: any,
-        columns: any[],
+        passing: unknown,
+        columns: unknown[],
         alias?: TableAlias,
     },
     SemanticView?: {
@@ -314,13 +586,13 @@ export interface TableAliasColumnDef {
  */
 export interface TableFunctionArgs {
     args: FunctionArg[];
-    settings?: any;
+    settings?: unknown;
 }
 
 /**
  * @see https://docs.rs/sqlparser/latest/sqlparser/ast/enum.TableVersion.html
  */
-export type TableVersion = any; // TODO:
+export type TableVersion = unknown; // TODO:
 
 /**
  * The source of values in a PIVOT operation.
@@ -351,7 +623,7 @@ export interface OrderByExpr {
     options: OrderByOptions;
 
     // TODO: Define proper type
-    will_fill?: any;
+    will_fill?: unknown;
 }
 
 /**
@@ -470,9 +742,9 @@ export interface WindowSpec {
  */
 export interface WindowFrame {
     // TODO: Define proper types
-    units: any;
-    start_bound: any;
-    end_bound?: any;
+    units: unknown;
+    start_bound: unknown;
+    end_bound?: unknown;
 }
 
 /**
